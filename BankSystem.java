65e5a57f6e2c6aadbfedad88b2b8a3348062acea
Aryan <aryan@gmail.com>
2026-03-24 14:14:17 +0530

    msg

---
+++ BankSystem.java
@@ -0,0 +1,41 @@
+// Custom Exception
+class InsufficientBalanceException extends Exception {
+    public InsufficientBalanceException(String message) {
+        super(message);
+    }
+}
+
+class BankAccount {
+    double balance = 1000;
+
+    void withdraw(double amount) throws InsufficientBalanceException {
+
+        if (amount < 0) {
+            throw new IllegalArgumentException("Invalid amount!");
+        }
+
+        if (amount > balance) {
+            throw new InsufficientBalanceException("Insufficient balance!");
+        }
+
+        balance -= amount;
+        System.out.println("Withdrawal successful, new balance: " + balance);
+    }
+}
+
+public class BankSystem {
+    public static void main(String[] args) {
+
+        BankAccount acc = new BankAccount();
+
+        try {
+            acc.withdraw(500);
+
+        } catch (InsufficientBalanceException e) {
+            System.out.println(e.getMessage());
+
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
